a073a53dd35cac232da5d3e94459168d2485aa7f
alu0100825503 <alu0100825503@ull.edu.es>
2016-05-22 08:39:43 +0000

    añadiendo funcionalidad al boton descargar

--- public/main.js
+++ public/main.js
@@ -27,10 +27,18 @@
       }
     });
     
-    $('#download').click(function(e) {
-      e.preventDefault();  //stop the browser from following
-      window.location.href = 'uploads/file.doc';
-    });
+    function download(filename, text) {
+      var element = document.createElement('a');
+      element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+      element.setAttribute('download', filename);
+    
+      element.style.display = 'none';
+      document.body.appendChild(element);
+    
+      element.click();
+    
+      document.body.removeChild(element);
+    }
   
     // volcando contenido del fichero seleccionado al textarea
     $("#examples").change(function(ev) {
